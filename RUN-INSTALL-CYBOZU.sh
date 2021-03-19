c739523be0b214f93c57e10aeee1f6b43c2e3584
krsuhjunho <krsuhjunho@gmail.com>
2021-03-19 13:50:37 +0000

    2021-03-19 Cybozu Dockerfile

--- RUN-INSTALL-CYBOZU.sh
+++ RUN-INSTALL-CYBOZU.sh
@@ -28,7 +28,7 @@ ECHO_MESSAGE "CYBOZU VER => ${CYBOZU_VER}"
 #Download File
 cd ${APACHE_PATH}
 wget https://download.cybozu.co.jp/office10/cbof-${CYBOZU_VER}-linux.bin 
-bash "cbof-${CYBOZU_VER}-linux.bin"
+bash "${APACHE_PATH}/cbof-${CYBOZU_VER}-linux.bin"
 }
 
 

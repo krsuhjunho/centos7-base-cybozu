4047f2d48628ac8fc439648e8981afe1c6d9ef92
krsuhjunho <krsuhjunho@gmail.com>
2021-03-20 00:28:22 +0000

    2021-03-20 delete docker-build && edit docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#VAR
 DOCKER_CONTAINER_NAME="cybozu-test"
 CONTAINER_HOST_NAME="cybozu-test"
 SSH_PORT=22458
@@ -9,22 +10,78 @@ SERVER_IP=$(curl -s ifconfig.me)
 PC_URL="cgi-bin/cbag/ag.cgi"
 HTTP_BASE="http://"
 TODAY=$(date "+%Y-%m-%d")
-Comment="$1"
+TIME_ZONE="Asia/Tokyo"
+COMMIT_COMMENT="$2"
+BUILD_OPTION="$1"
 
+DOCKER_IMAGE_BUILD()
+{
+docker build -t ${BASE_IMAGE_NAME} .
+}
 
+DOCKER_IMAGE_PUSH()
+{
+docker push ${BASE_IMAGE_NAME}
+}
+
+GIT_COMMIT_PUSH()
+{
+git add . --ignore-removal
+git commit -m "${TODAY} ${COMMIT_COMMENT}"
+git config credential.helper store
+git push origin main
+}
+
+DOCKER_CONTAINER_REMOVE()
+{
+docker rm -f ${DOCKER_CONTAINER_NAME}
+}
+
+DOCKER_CONTAINER_CREATE()
+{
 docker run -tid --privileged=true \
 -h "${CONTAINER_HOST_NAME}" \
 --name="${DOCKER_CONTAINER_NAME}" \
 -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
 -v /etc/localtime:/etc/localtime:ro \
--e TZ=Asia/Tokyo \
+-e TZ=${TIME_ZONE} \
 -p ${SSH_PORT}:22 -p ${HTTP_PORT}:80 \
 ${BASE_IMAGE_NAME}
 
+}
 
+DOCKER_CONTAINER_BASH()
+{
 docker exec -it ${DOCKER_CONTAINER_NAME} /bin/bash /var/www/cbof-10.8.5-linux.bin
+}
 
+DOCKER_CONTAINER_URL_SHOW()
+{
 echo ""
 echo "CYBOZU URL => ${HTTP_BASE}${SERVER_IP}:${HTTP_PORT}/${PC_URL}"
 echo ""
+}
+
+MAIN()
+{
+
+if [ "$BUILD_OPTION" == "--build" ]; then
+    DOCKER_IMAGE_BUILD
+	DOCKER_IMAGE_PUSH
+	GIT_COMMIT_PUSH
+fi
+
+DOCKER_CONTAINER_REMOVE
+DOCKER_CONTAINER_CREATE
+DOCKER_CONTAINER_BASH
+DOCKER_CONTAINER_URL_SHOW
+}
+
+MAIN
+
+
+
+
+
+
 
